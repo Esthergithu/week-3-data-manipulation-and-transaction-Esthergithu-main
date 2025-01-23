eb665386b1127cc78da00eadc7cfaf9dcf5d3039
Esthergithu <ewanjikugithu@gmail.com>
2025-01-24 02:22:41 +0300

    second commit

--- answer.sql
+++ answer.sql
@@ -1,2 +1,20 @@
--- Write your answers here
+-- Question 1
+-- create the categories table
 
+CREATE TABLE categories (
+    categoryID INT PRIMARY KEY AUTO_INCREMENT,
+    categoryName VARCHAR(50) NOT NULL 
+);
+
+-- Question 2
+-- Insert categories into the table
+INSERT INTO categories (categoryName)
+VALUES
+    ('Utilities'),
+    ('Rent'),
+    ('Groceries'),
+    ('Transportation'),
+    ('Entertainment');
+    
+ -- Question 3
+ 
